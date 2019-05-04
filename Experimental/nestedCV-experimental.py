aa9d54566c25e9a8b1102ea50564fc4888eadcc0
casperbh96 <casperbh.96@gmail.com>
2019-05-04 14:37:50 +0200

    Added nested_grid_search function, not done

--- Experimental/nestedCV-experimental.py
+++ Experimental/nestedCV-experimental.py
@@ -14,6 +14,8 @@ from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
 import numpy as np
+from grid_search import NestedGridSearchCV
+from mpi4py import MPI
 
 train = pd.read_csv('./data/train.csv')
 test = pd.read_csv('./data/test.csv')
@@ -43,8 +45,46 @@ def data_engineering(train, test):
     
     return X_train,X_test,y
 
+def nested_grid_search(X,y,estimator, param_grid, scoring=None, outer_cv=None, inner_cv=None):
+    for i, (train_index_outer, test_index_outer) in enumerate(outer_cv.split(X,y)):
+        X_train, X_test = X[train_index_outer, :], X[test_index_outer, :]
+        y_train, y_test = y[train_index_outer], y[test_index_outer]
+        
+        #For integer/None inputs, if classifier is True and y is either
+        #binary or multiclass, StratifiedKFold is used.
+        #In all other cases, KFold is used.
+        cv_strategy = check_cv(inner_cv, y_train, classifier=is_classifier(estimator))
+        
+        for j, (train_index_inner, test_index_inner) in enumerate(cv_strategy.split(X_train,y_train)):
+            X_train_val, X_test_val = X_train[train_index_inner, :], X_train[test_index_inner, :]
+            y_train_val, y_test_val = y_train[train_index_inner], y_train[test_index_inner]
+            
+            #hhttps://github.com/scikit-learn/scikit-learn/blob/0.21.X/sklearn/model_selection/_validation.py#L393
+            testing = _fit_and_score(estimator=clone(estimator),
+                                 X=X_train_val, y=y_train_val,
+                                 scorer=scoring, train=train_index_inner,
+                                 test=test_index_inner, fit_parameters=param_grid)
+        
+
 X,X_test,y = data_engineering(train,test)
 
+estimator = RandomForestRegressor()
+
+param_grid = {'max_depth': [3, None],
+              'n_estimators': (10, 20)#, 30, 50, 100, 200, 400, 600, 800, 1000)
+}
+
+nested_cv = NestedGridSearchCV(estimator, param_grid, 'neg_mean_squared_error',
+                               cv=3, inner_cv=3)
+nested_cv.fit(X, y)
+
+if MPI.COMM_WORLD.Get_rank() == 0:
+    for i, scores in enumerate(nested_cv.grid_scores_):
+        scores.to_csv('grid-scores-%d.csv' % (i + 1), index=False)
+
+    print(nested_cv.best_params_)
+
+'''
 NUM_TRIALS = 30
 
 p_grid = {'max_depth': [3, None],
@@ -83,7 +123,7 @@ plt.title("Generalization Error with Random Forest",
           x=1, y=1.1, fontsize="15")
 plt.show()
 
-'''
+
 outer_scores = []
 
 # outer cross-validation

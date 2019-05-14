cee2fae88711839926fae7cfe654d122822e6739
Ahmed Magdy <ahmedmagdi@outlook.com>
2019-05-14 23:05:06 +0200

    rewrite best_inner_cv loop

--- Experimental/nested_cv.py
+++ Experimental/nested_cv.py
@@ -32,25 +32,26 @@ def nested_cv(X, y, model, params_grid, outer_kfolds,
     inner_kfolds : int
         Number of inner K-partitions in KFold
 
-    metric : callable from sklearn.metrics, default = mean_squared_error
-        A scoring metric used to score each model
-
-    lower_score_is_better : boolean, default = True
-        Whether or not higher or lower is better for metric
-
-    sqrt_of_score : boolean, default = False
-        Whether or not if the square root should be taken of score
-
-    randomized_search : boolean, default = True
-        Whether to use gridsearch or randomizedsearch from sklearn
-
-    randomized_search_iter : int, default = 10
-        Number of iterations for randomized search
-
-    do_recursive_feature_elimination : boolean, default = False
-        Whether to do feature elimination
     cv_options: dict, default = {}
         Nested CV Options, check docs for details.
+
+        metric : callable from sklearn.metrics, default = mean_squared_error
+            A scoring metric used to score each model
+
+        metric_score_indicator_lower : boolean, default = True
+            Choose whether lowe score is better for the metric calculation or hight score is better, `True` means lower score is better.
+
+        sqrt_of_score : boolean, default = False
+            Whether or not if the square root should be taken of score
+
+        randomized_search : boolean, default = True
+            Whether to use gridsearch or randomizedsearch from sklearn
+
+        randomized_search_iter : int, default = 10
+            Number of iterations for randomized search
+
+        do_recursive_feature_elimination : boolean, default = False
+            Whether to do feature elimination
 
     Returns
     -------
@@ -63,15 +64,19 @@ def nested_cv(X, y, model, params_grid, outer_kfolds,
     best_inner_params_list
          Best inner params for each outer loop
     '''
-    x = {}
 
-    metric = cv_options. get('metric', mean_squared_error)
-    lower_score_is_better = cv_options. get('lower_score_is_better', True)
-    sqrt_of_score = cv_options. get('sqrt_of_score', False)
-    randomized_search = cv_options. get('randomized_search', True)
-    randomized_search_iter = cv_options. get('randomized_search_iter', 10)
-    do_recursive_feature_elimination = cv_options. get(
-        'do_recursive_feature_elimination', False)
+    metric = cv_options.get('metric', mean_squared_error)
+    metric_score_indicator_lower = cv_options.get('metric_score_indicator_lower', True)
+    sqrt_of_score = cv_options.get('sqrt_of_score', False)
+    randomized_search = cv_options.get('randomized_search', True)
+    randomized_search_iter = cv_options.get('randomized_search_iter', 10)
+    do_recursive_feature_elimination = cv_options.get('do_recursive_feature_elimination', False)
+
+    # TODO: Convert the big function into class.
+    def transform_score_format(scoreValue):
+        if sqrt_of_score:
+            return np.sqrt(scoreValue)
+        return scoreValue
 
     print('\n{0} <-- Running this model now'.format(type(model).__name__))
     outer_cv = KFold(n_splits=outer_kfolds, shuffle=True)
@@ -79,10 +84,10 @@ def nested_cv(X, y, model, params_grid, outer_kfolds,
 
     outer_scores = []
     variance = []
-    best_inner_params_list = []
+    best_inner_params_list = []  # Change both to by one thing out of key-value pair
     best_inner_score_list = []
 
-    # Split X and y into K-partitions
+    # Split X and y into K-partitions to Outer CV
     for (i, (train_index, test_index)) in enumerate(outer_cv.split(X, y)):
         print('\n{0}/{1} <-- Current outer fold'.format(i+1, outer_kfolds))
         X_train_outer, X_test_outer = X.iloc[train_index], X.iloc[test_index]
@@ -92,15 +97,15 @@ def nested_cv(X, y, model, params_grid, outer_kfolds,
         best_inner_params = {}
         best_inner_score = None
 
-        # Split X_train_outer and y_train_outer into K-partitions
+        # Split X_train_outer and y_train_outer into K-partitions to be inner CV
         for (j, (train_index_inner, test_index_inner)) in enumerate(inner_cv.split(X_train_outer, y_train_outer)):
             print('\n\t{0}/{1} <-- Current inner fold'.format(j+1, inner_kfolds))
             X_train_inner, X_test_inner = X_train_outer.iloc[
                 train_index_inner], X_train_outer.iloc[test_index_inner]
             y_train_inner, y_test_inner = y_train_outer.iloc[
                 train_index_inner], y_train_outer.iloc[test_index_inner]
-            best_score = None
-            best_grid = {}
+            best_inner_score = None
+            best_inner_grid = {}
 
             # Run either RandomizedSearch or GridSearch for input parameters
             for param_dict in ParameterSampler(param_distributions=params_grid, n_iter=randomized_search_iter) if randomized_search else ParameterGrid(param_grid=params_grid):
@@ -108,34 +113,24 @@ def nested_cv(X, y, model, params_grid, outer_kfolds,
                 model.set_params(**param_dict)
                 model.fit(X_train_inner, y_train_inner)
                 inner_pred = model.predict(X_test_inner)
-                internal_grid_score = metric(y_test_inner, inner_pred)
+                inner_grid_score = metric(y_test_inner, inner_pred)
+                current_inner_score_value = best_inner_score
 
                 # Find best score and corresponding best grid
-                if (best_score == None or internal_grid_score < best_score) and (lower_score_is_better):
-                    if sqrt_of_score:
-                        best_score = np.sqrt(internal_grid_score)
-                    else:
-                        best_score = internal_grid_score
-                    best_grid = param_dict
-                elif (best_score == None or internal_grid_score > best_score) and (not lower_score_is_better):
-                    if sqrt_of_score:
-                        best_score = np.sqrt(internal_grid_score)
-                    else:
-                        best_score = internal_grid_score
-                    best_grid = param_dict
+                if(best_inner_score is not None):
+                    if(metric_score_indicator_lower and  best_inner_score > inner_grid_score):
+                        best_inner_score = transform_score_format(inner_grid_score)
+                    elif (not metric_score_indicator_lower and best_inner_score < inner_grid_score):
+                        best_inner_score = transform_score_format(inner_grid_score)
+                else:
+                    best_inner_score = transform_score_format(inner_grid_score)
+                # Update best_inner_grid once rather than calling it under each if statement 
+                if(current_inner_score_value is not None and current_inner_score_value != best_inner_score):
+                    best_inner_grid = param_dict
 
             # Best grid and score found by the search
-            inner_params.append(best_grid)
-            inner_scores.append(best_score)
-
-        # Look through all inner scores, select the best one
-        for idx, score in enumerate(inner_scores):
-            if (best_inner_score == None or score < best_inner_score) and (lower_score_is_better):
-                best_inner_score = score
-                best_inner_params = inner_params[idx]
-            elif (best_inner_score == None or score > best_inner_score) and (not lower_score_is_better):
-                best_inner_score = score
-                best_inner_params = inner_params[idx]
+            inner_params.append(best_inner_grid)
+            inner_scores.append(best_inner_score)
 
         best_inner_params_list.append(best_inner_params)
         best_inner_score_list.append(best_inner_score)
